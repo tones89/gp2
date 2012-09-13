0042dcf572d0f39e2f5afad22afa8d9a86b37c05
Anthony Denovan <adenov200@caledonian.ac.uk>
2012-09-13 12:49:55 +0100

    Created Game Application.cpp file and began work

---
+++ GP2/GameApplication.cpp
@@ -0,0 +1,9 @@
+#include "GameApplication.h"
+
+CGameApplication::CGameApplication(void)
+{
+	m_pWindow=NULL;
+	m_pD3D10Device=NULL;
+	//m_pRenderTargetView=NULL;
+	m_pSwapChain=NULL;
+}
---
+++ GP2/GameApplication.h
@@ -0,0 +1,28 @@
+#pragma once 
+
+#include "Win32Window.h"
+#include <D3D10.h>
+#include <D3DX10.h>
+
+class CGameApplication
+{
+public:
+	CGameApplication(void);
+	~CGameApplication(void);
+
+	bool init();
+	bool run();
+
+private:
+	bool initGraphics();
+	bool initWindow();
+
+	void render();
+	void update();
+
+private: 
+	ID3D10Device * m_pD3D10Device;
+	IDXGISwapChain * m_pSwapChain;
+	ID3D10RenderTargetView * m_pWindow;
+
+};
