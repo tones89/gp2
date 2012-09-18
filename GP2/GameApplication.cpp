67d38d9103b80d89c56b3f3680223fb5d05f5a57
Anthony Denovan <adenov200@caledonian.ac.uk>
2012-09-18 20:34:39 +0100

    Finished the init graphics method

--- GP2/GameApplication.cpp
+++ GP2/GameApplication.cpp
@@ -50,12 +50,86 @@ void CGameApplication::run()
 	}
 }
 
+void CGameApplication::update()
+{
+}
+
+bool CGameApplication::initGraphics()
+{
+	RECT windowRect;
+
+	GetClientRect(m_pWindow->getHandleToWindow(),&windowRect);
+
+	UINT width = windowRect.right-windowRect.left;
+	UINT height = windowRect.bottom-windowRect.top;
+
+	UINT createDeviceFlags = 0;
+#ifdef _DEBUG
+		createDeviceFlags|=D3D10_CREATE_DEVICE_DEBUG;
+#endif
+		DXGI_SWAP_CHAIN_DESC sd;
+		ZeroMemory( &sd, sizeof(sd));
+
+		if (m_pWindow->isFullScreen())
+			sd.BufferCount = 2;
+		else
+			sd.BufferCount = 1;
+
+		sd.OutputWindow = m_pWindow->getHandleToWindow();
+		sd.Windowed =(BOOL)(!m_pWindow->isFullScreen());
+		sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+
+		sd.SampleDesc.Count = 1;
+		sd.SampleDesc.Quality = 0;
+
+		sd.BufferDesc.Width = width;
+		sd.BufferDesc.Height = height;
+		sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		sd.BufferDesc.RefreshRate.Numerator=60;
+		sd.BufferDesc.RefreshRate.Denominator=1;
+
+		
+		if(FAILED(D3D10CreateDeviceAndSwapChain(NULL,D3D10_DRIVER_TYPE_HARDWARE,NULL,createDeviceFlags,
+			D3D10_SDK_VERSION,&sd,&m_pSwapChain,&m_pD3D10Device)))
+			return false;
+		
+		ID3D10Texture2D *pBackBuffer;
+		if(FAILED(m_pSwapChain->GetBuffer(0,__uuidof(ID3D10Texture2D),(void**)&pBackBuffer)))
+			return false;
+
+		if(FAILED(m_pD3D10Device->CreateRenderTargetView(pBackBuffer,NULL,&m_pRenderTargetView)))
+		{
+			pBackBuffer->Release();
+			return false;
+		}
+		pBackBuffer->Release();
+
+		m_pD3D10Device->OMSetRenderTargets(1,&m_pRenderTargetView,NULL);
+
+		D3D10_VIEWPORT vp;
+		vp.Width = width;
+		vp.Height = height;
+		vp.MinDepth = 0.0f;
+		vp.MaxDepth = 1.0f;
+		vp.TopLeftX = 0;
+		vp.TopLeftY = 0; 
+		m_pD3D10Device->RSSetViewports(1,&vp);
+
+	return true;
+}
+
 void CGameApplication::render()
 {
 }
 
 bool  CGameApplication::initWindow()
 {
+	m_pWindow=new CWin32Window();
+
+	if (!m_pWindow->init(TEXT("Lab1-Create Device"),800,640,false))
+		return false;
+
+
 	return true;
 }
 
